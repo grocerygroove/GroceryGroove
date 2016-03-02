2a6f971fcc9ba2937d94f851768798b34d7d9626
jjohnson338 <jaredj@gmx.com>
2016-03-01 21:22:03 -0500

    Got api working a bit

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,12 +1,10 @@
 //DB setup
 const pg = require('pg');
-const client = new pg.Client({
-  user: 'grocerygroove_owner',
-  password: 'oq9urqwe09r8yr89q432yr8huirhgjbvh',
-  database: 'grocerygroove',
-  host: 'localhost',
-  port: '5432'
-});
+const connString = "postgres://grocerygroove_owner:oq9urqwe09r8yr89q432yr8huirhgjbvh@localhost:5432/grocerygroove";
+const getClient = function(cb){
+  pg.connect(connString, cb);
+};
+
 
 //Express setup
 const express = require('express');
@@ -15,13 +13,52 @@ const app = express();
 app.use(bodyParser.json());
 
 //Routes
-app.post('/user', function(req, res){
-  client.connect();
-  client.query('SELECT create_user($1, $2)', [req.body.email, req.body.password], function(err){
-    if(err)
-      console.log(err);
-    res.send("Failed");
+app.post('/user', (req, res, next) => {
+  getClient((err, client, done) => {
+    if(err){
+      next(err);
+      return;
+    }
+
+    client.query('SELECT create_user($1, $2)', [req.body.email, req.body.password], (err) => {
+      done();
+      if (err){
+        next(err);
+        return;
+      }
+      else {
+        res.send("All good");
+      }
+    });
   });
+});
+
+app.get('/user/household_id', (req, res, next) => {
+  getClient((err, client, done) => {
+    if(err){
+      next(err);
+      return;
+    }
+
+    client.query('SELECT household_id FROM users WHERE email = $1', [req.body.email], (err, result) => {
+      done();
+      if (err){
+        next(err);
+        return;
+      }
+      else {
+        res.send(result);
+      }
+    });
+  });
+});
+
+
+
+
+app.use((error, req, res, next) => {
+    res.sendStatus(503);
+    console.log(error);
 });
 
 app.listen(8080);
