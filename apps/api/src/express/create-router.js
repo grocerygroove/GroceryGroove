acb479f1217e269dd6e46b2c29e5594ae7a2454f
Shawn Dellysse <sdellysse@gmail.com>
2016-06-26 19:42:01 -0400

    added possibly more elegant router creation

--- apps/api/src/express/create-router.js
+++ apps/api/src/express/create-router.js
@@ -1,1 +1,11 @@
-module.exports = require("express").Router;
+const express = require("express");
+
+module.exports = function createRouter (cb) {
+    const retval = express.Router();
+
+    if (cb) {
+        cb(retval);
+    }
+
+    return retval;
+};
