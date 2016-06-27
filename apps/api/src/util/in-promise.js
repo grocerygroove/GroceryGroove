5072ab7aa5777c91272881ed895d3df4f6255a0e
Shawn Dellysse <sdellysse@gmail.com>
2016-06-26 21:13:41 -0400

    removed in-promise for now

--- apps/api/src/util/in-promise.js
+++
@@ -1,9 +0,0 @@
-module.exports = function inPromise (func) {
-    return new Promise((resolve, reject) => {
-        try {
-            resolve(func());
-        } catch (e) {
-            reject(e);
-        }
-    });
-};
