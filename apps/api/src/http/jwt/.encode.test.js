ab03af610e0872eda85bd20b4a045cc3641365ef
Shawn Dellysse <sdellysse@gmail.com>
2016-06-29 22:44:52 -0400

    cleaned up encode test

--- apps/api/src/http/jwt/.encode.test.js
+++ apps/api/src/http/jwt/.encode.test.js
@@ -10,35 +10,38 @@ var test = require('tape');
 test('encode tests', function (t) {
     t.plan(3);
 
-    //Shared vars
-    const testSecretKey = 'thisisthetestsecretkey';
-    const email = 'test@test.com';
-    const expectedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJjcmVhdGVkX2RhdGUiOjkxNTE0ODgwMDAwMH0.bHOlRiKoIpweT4Mpk7n-2GwaK3jiPAE-2Bp6pp-ZM50';
-
-    const test1Actual = function(){
-        const created_date = moment('1999-01-01').valueOf();
-
-        return encode(testSecretKey, email, created_date);
-    }();
-
-    const test1Expected = function(){
-        return expectedToken;
-    }();
-    t.equal(test1Actual, test1Expected, `
-        should be equal - know token vs generated token
-        with same payload
-    `);
-
-    const test2Actual = function(){
-        return encode(testSecretKey, email);
-    }();
-    const test2Expected = function(){
-        return expectedToken;
-    }();
-    t.notEqual(test2Actual, test2Expected, `
-        should not be equal - known token vs generated token with
-        automatic expiration date
-    `);
-
     t.equal(typeof encode, "function");
+
+    (function () {
+        const testSecretKey = 'thisisthetestsecretkey';
+        const email = 'test@test.com';
+        const expectedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJjcmVhdGVkX2RhdGUiOjkxNTE0ODgwMDAwMH0.bHOlRiKoIpweT4Mpk7n-2GwaK3jiPAE-2Bp6pp-ZM50';
+
+        const actual = (function(){
+            const created_date = moment('1999-01-01').valueOf();
+
+            return encode(testSecretKey, email, created_date);
+        })();
+
+        const expected = expectedToken;
+
+        t.equal(actual, expected, `
+            should be equal - know token vs generated token with same payload
+        `);
+    })();
+
+    (function () {
+        const testSecretKey = 'thisisthetestsecretkey';
+        const email = 'test@test.com';
+        const expectedToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJjcmVhdGVkX2RhdGUiOjkxNTE0ODgwMDAwMH0.bHOlRiKoIpweT4Mpk7n-2GwaK3jiPAE-2Bp6pp-ZM50';
+
+        const actual = encode(testSecretKey, email);
+        const expected = expectedToken;
+
+        t.notEqual(actual, expected, `
+            should not be equal - known token vs generated token with automatic
+            expiration date
+        `);
+
+    })();
 });
