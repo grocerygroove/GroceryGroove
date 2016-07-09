60107c6bb7a813ce58eede16a004a9e35c4c1c7b
Shawn Dellysse <sdellysse@gmail.com>
2016-07-09 15:04:21 -0400

    added household_id extractor

---
+++ apps/api/src/middleware/create-household-extractor.js
@@ -0,0 +1,24 @@
+const a = require("../utils/asyncify");
+
+module.exports = function createHouseholdExtractor (logger) {
+    logger = logger.child({
+        middleware: "household-extractor",
+    });
+
+    return a(function* (ctx, next) {
+        if (ctx.query.household_id && ctx.query.household_id.match(/^\d+$/)) {
+            ctx.state.household_id = parseInt(ctx.query.household_id, 10);
+
+            return (yield next());
+        } else {
+            logger.info({
+                errorName: "Failed household_id check",
+                household_id: ctx.query.household_id,
+            });
+            ctx.status = 424;
+            ctx.body = "Invalid household_id";
+
+            return;
+        }
+    });
+};
