e8e99b9f15a1602cdae327c420ef006ea5ecdb2f
Shawn Dellysse <sdellysse@gmail.com>
2016-07-09 16:45:23 -0400

    extracted assign into collapseQueries in make-query-functions

--- apps/api/src/db/make-query-functions-from-directory.js
+++ apps/api/src/db/make-query-functions-from-directory.js
@@ -12,41 +12,43 @@ const transformSqlError = require("./make-query-functions-from-directory/transfo
 const isDirectory = (path => statSync(path).isDirectory());
 const stripExtension = (filename => filename.split(".").slice(0, -1).join("."));
 
+/**
+ * Helper function to help deal with the issue of having a
+ * `${path}/something.js` file and a bunch of files under the
+ * `${path}/something/` directory. This handles that by looking to see when you
+ * try to assign an item to a name, if one of them is an object (a directory)
+ * and one of the is a function (a file), then it applies all of the object's
+ * properties to the function and sets that function in the place of `name`.
+ *
+ * NOTE: This function modifies its arguments.
+ */
+const collapseQueries = function (queries, name, item) {
+    if (queries[name]) {
+        if (typeof(queries[name]) === typeof(item)) {
+            throw new Error(`Duplicate name: "${ name }"`);
+        }
+
+        switch (typeof queries[name]) {
+            case "object":
+                queries[name] = Object.assign(item, queries[name]);
+            break;
+
+            case "function":
+                Object.assign(queries[name], item);
+            break;
+
+            default:
+                throw new Error(`unknown type "${ typeof queries[name] }" for "${ name }"`);
+            break;
+        }
+    } else {
+        queries[name] = item;
+    }
+};
+
 module.exports = function makeQueryFunctionsFromDirectory (path) {
     const retval = {};
-
-    /**
-     * Helper function to help deal with the issue of having a
-     * `${path}/something.js` file and a bunch of files under the
-     * `${path}/something/` directory. This handles that by looking to see when
-     * you try to assign an item to a name, if one of them is an object (a
-     * directory) and one of the is a function (a file), then it applies all of
-     * the object's properties to the function and sets that function in the
-     * place of `name`.
-     */
-    const assign = function (name, item) {
-        if (retval[name]) {
-            if (typeof(retval[name]) === typeof(item)) {
-                throw new Error(`Duplicate name: "${ name }"`);
-            }
-
-            switch (typeof retval[name]) {
-                case "object":
-                    retval[name] = Object.assign(item, retval[name]);
-                break;
-
-                case "function":
-                    Object.assign(retval[name], item);
-                break;
-
-                default:
-                    throw new Error(`unknown type "${ typeof retval[name] }" for "${ name }"`);
-                break;
-            }
-        } else {
-            retval[name] = item;
-        }
-    };
+    const assign = (name, item) => collapseQueries(retval, name, item);
 
     for(const filename of readDirSync(path)){
         const pathname = `${ path }/${ filename }`;
