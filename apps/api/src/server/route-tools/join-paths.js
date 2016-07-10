1beb246db0ce970ee7273650b9d818376b238a55
Shawn Dellysse <sdellysse@gmail.com>
2016-07-10 17:12:03 -0400

    removed vestigial code

--- apps/api/src/server/route-tools/join-paths.js
+++ apps/api/src/server/route-tools/join-paths.js
@@ -1,8 +1,3 @@
-const stripSurroundingSlashes = function (path) {
-    return path
-    ;
-};
-
 module.exports = function joinPaths (...paths) {
     paths = paths
         .map(path => path.replace(/^\//, ''))
