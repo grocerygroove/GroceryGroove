6fbcc8c144ed5de8854252b243ecf3eb15752bd0
Shawn Dellysse <sdellysse@gmail.com>
2016-07-10 17:10:23 -0400

    better error checking in user extractor

--- apps/api/src/middleware/create-user-extractor.js
+++ apps/api/src/middleware/create-user-extractor.js
@@ -6,8 +6,8 @@ module.exports = function createUserExtractor (logger) {
     });
 
     return a(function* (ctx, next) {
-        if (!ctx.state.token || !ctx.state.token.data.userId) {
-            logger.info({
+        if (!ctx.state.token || !ctx.state.token.data || !ctx.state.token.data.userId) {
+            logger.error({
                 errorName: "userId missing from token, possible forgot to use jwtAuthMw before this one?",
                 token: ctx.state.token,
             });
