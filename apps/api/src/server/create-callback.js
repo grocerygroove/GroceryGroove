d2a5e760fa79fe82966e933a97bb0018bd5468e9
Shawn Dellysse <sdellysse@gmail.com>
2016-07-09 23:40:22 -0400

    pppush it reall good do de doo do

--- apps/api/src/server/create-callback.js
+++ apps/api/src/server/create-callback.js
@@ -2,19 +2,16 @@ const createHouseholdExtractor = require("../middleware/create-household-extract
 const createJsonBodyParser = require("koa-json-body");
 const createRouter = require("../http/create-router");
 const Koa = require('koa');
+const routes = {
+    login: {
+        postByEmail:            require("./routes/login/post-by-email"),
+        postByDeviceIdentifier: require("./routes/login/post-by-device-identifier"),
+    },
+};
 
-/**
- * Put these routes in order of most-used to least-used, as each incoming
- * request will scan the entire list before finding the route.
- */
-const routes = [
-    { path: "/grocery-lists",  routerCreator: require("./routes/grocery-lists")  },
-    { path: "/households",     routerCreator: require("./routes/households")     },
-    { path: "/categories",     routerCreator: require("./routes/categories")     },
-    { path: "/quantity-types", routerCreator: require("./routes/quantity-types") },
-    { path: "/login",          routerCreator: require("./routes/login")          },
-    { path: "/signup",         routerCreator: require("./routes/signup")         },
-];
+const createRoutes = function (cb) {
+    return createRouter(cb).routes();
+};
 
 module.exports = function createCallback (services) {
     if (typeof services !== "object") {
@@ -38,12 +35,38 @@ module.exports = function createCallback (services) {
 
     // I really don't like this. I need to fully investigate routing in koa
     // but for now this will work.
-    koaApp.use(createRouter(r => {
-        for (const { path, routerCreator } of routes) {
-            const router = routerCreator(services);
-            r.use(path, router.routes());
-        }
-    }).routes());
+    koaApp.use(createRoutes(r => {
+        r.use("/grocery-lists",  r => require("./routes/grocery-lists")  (services));
+        r.use("/households",     r => require("./routes/households")     (services));
+        r.use("/categories",     r => require("./routes/categories")     (services));
+        r.use("/quantity-types", r => require("./routes/quantity-types") (services));
+
+        r.use("/login", createRoutes(r => {
+            r.use(services.jsonBodyParserMw);
+
+            r.post("/by-email", (ctx, next) => routes.login.postByEmail(
+                services.db,
+                services.jwtService,
+                services.logger.child({
+                    route: "/login/by-email",
+                }),
+                ctx,
+                next
+            ));
+
+            r.post("/by-device-identifer", (ctx, next) => routes.login.postByDeviceIdentifer(
+                services.db,
+                services.jwtService,
+                services.logger.child({
+                    route: "/login/by-device-identifier",
+                }),
+                ctx,
+                next
+            ));
+        }));
+
+        r.use("/signup",         r => require("./routes/signup")         (services));
+    }));
 
     return koaApp.callback();
 };
