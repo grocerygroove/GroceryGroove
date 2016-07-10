ee57c2016088150e1ee07b9538767c31370cdabb
Jared Johnson <jaredj@gmx.com>
2016-07-10 19:49:27 -0400

    Got the route test working...mostly...there is a bug

--- apps/api/src/server/routes/.login.test.js
+++ apps/api/src/server/routes/.login.test.js
@@ -4,9 +4,8 @@ const a = require("../../utils/asyncify");
 const getRoute = require("../route-tools/get-route");
 const rootGroup = require("../routes");
 
-test("server/routes/login", a(function* (t){
-
-    t.plan(1);
+test("server/routes/login", a(function* (t) {
+    t.plan(2);
 
     (function(){
         //Setup
@@ -22,12 +21,12 @@ test("server/routes/login", a(function* (t){
         };
 
         let db = {
-            query : function(logger, {name}){
-                if(name === "checkByEmail")
+            query : a(function*(logger, {name}){
+                if(name === "users/check-by-email")
                 {
-                    return Promise.resolve(1);//UserId value
+                    return Promise.resolve([{ userId : 1}]);//UserId value
                 }
-            },
+            }),
         };
 
         let logger = {};
@@ -37,8 +36,52 @@ test("server/routes/login", a(function* (t){
                 return {userId};
             },
         };
+
         methodToTest(db, jwtService, logger, ctx, next).then(()=>{
-            t.equals(ctx.body.token.userId, 1);
+            const actual = ctx.body.token.userId;
+            const expected = 1;
+            t.equal(actual, expected, `
+                Valid row set returned from the db, should encode the
+                payload and inject in into the ctx.body.token property`);
+        });
+    })();
+
+    (function(){
+        //Setup
+        const methodToTest = getRoute(rootGroup, "POST", "/login/by-email").handler;
+
+        let ctx = {
+            request : {
+                body : {
+                    email : "test@test.com",
+                    password : "testpass",
+                }
+            }
+        };
+
+        let db = {
+            query : a(function*(logger, {name}){
+                if(name === "users/check-by-email")
+                {
+                    return Promise.resolve([]);//Empty result set array
+                }
+            }),
+        };
+
+        let logger = {};
+        let next = () => {};
+        let jwtService = {
+            encode : function({userId}){
+                return {userId};
+            },
+        };
+
+        methodToTest(db, jwtService, logger, ctx, next).then(()=>{
+            const actual = ctx.status;
+            const expected = 403;
+            t.equal(actual, expected, `
+                If no rows are returned,
+                the ctx response status should be 403`);
         });
     })();
 
