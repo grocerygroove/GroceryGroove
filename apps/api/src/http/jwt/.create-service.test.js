8de73fbeea9a1884b4e7453a7bbc412a63875d51
Shawn Dellysse <shawnd@omniti.com>
2016-07-11 19:27:06 -0400

    added more test to jwt-service

--- apps/api/src/http/jwt/.create-service.test.js
+++ apps/api/src/http/jwt/.create-service.test.js
@@ -1,11 +1,19 @@
 const createService = require("./create-service");
+const encode = require("./encode");
 const moment = require("moment");
 const tap = require("tap");
 
 tap.test("http/jwt/create-service", tap => {
-    const service = createService("some secret here");
+    const secret = "some secret here";
+    const service = createService(secret);
 
     tap.equal(typeof service.encode, "function", "encode should exist");
+    tap.equal(
+        encode(secret, { tony: "hawk" }, moment("1991-04-19").valueOf()),
+        service.encode({ tony: "hawk" }, moment("1991-04-19").valueOf()),
+        "service should just partially apply secret"
+    );
+
     tap.equal(typeof service.decode, "function", "decode should exist");
 
     tap.end();
