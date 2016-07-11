d7ae7c781e3f8714b315cf315a23b25599ea583a
Shawn Dellysse <shawnd@omniti.com>
2016-07-11 19:17:30 -0400

    only test the functionality of the target, not its dependencies if you can

--- apps/api/src/http/jwt/.create-service.test.js
+++ apps/api/src/http/jwt/.create-service.test.js
@@ -1,33 +1,14 @@
-const test = require("tap").test;
+const tap = require("tap");
 
 const a = require("../../utils/asyncify");
 const createService = require("./create-service");
 const moment = require("moment");
 
-test("http/jwt/create-service", a(function* (t) {
+tap.test("http/jwt/create-service", tap => {
     const service = createService("some secret here");
-    t.equal(typeof service.encode, "function");
-    t.equal(typeof service.decode, "function");
 
-    const created_date = moment('1999-01-01').valueOf();
-    const data = {
-            userId: 20,
-        };
+    tap.equal(typeof service.encode, "function", "encode should exist");
+    tap.equal(typeof service.decode, "function", "decode should exist");
 
-    let actual = function(){
-        const checkAgainstDate = moment('2000-01-01').valueOf();
-        const encodedToken = service.encode(data, created_date);
-        return service.decode(checkAgainstDate, encodedToken);
-    }();
-
-    let expected = function(){
-        return {
-            data : {
-                userId : 20,
-            },
-            created_date
-        };
-    }();
-
-    t.deepEqual(actual, expected);
-}));
+    tap.end();
+});
