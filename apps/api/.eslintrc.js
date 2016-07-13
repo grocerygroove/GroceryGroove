5e50b73b719f564082fdc8934ba1ad23d653dfd7
Shawn Dellysse <shawnd@omniti.com>
2016-07-13 17:42:41 -0400

    clearer eslint rules

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -6,9 +6,10 @@ module.exports = {
     parserOptions: {
         sourceType: "module",
     },
+
+    // rules should be in order of http://eslint.org/docs/rules
     rules: {
         // Possible Errors
-        "comma-dangle": ["error", "always-multiline"],
         "no-cond-assign": ["error", "always"],
         "no-console": ["error"],
         "no-constant-condition": ["error"],
@@ -28,17 +29,20 @@ module.exports = {
         "no-irregular-whitespace": ["error"],
         "no-negated-in-lhs": ["error"],
         "no-obj-calls": ["error"],
+        "no-prototype-builtins": ["error"],
         "no-regex-spaces": ["error"],
         "no-sparse-arrays": ["error"],
         "no-unexpected-multiline": ["error"],
         "no-unreachable": ["error"],
         "use-isnan": ["error"],
+        "valid-jsdoc": ["off"],
         "valid-typeof": ["error"],
 
         // Best Practices
         "accessor-pairs": ["error"],
         "array-callback-return": ["error"],
         "block-scoped-var": ["error"],
+        //"complexity"
         "consistent-return": ["error"],
         "curly": ["error", "all"],
         "default-case": ["error"],
@@ -50,10 +54,14 @@ module.exports = {
         "no-caller": ["error"],
         "no-case-declarations": ["error"],
         "no-div-regex": ["error"],
+        //"no-else-return"
+        //"no-empty-function"
         "no-empty-pattern": ["error"],
+        //"no-eq-null"
         "no-eval": ["error"],
         "no-extend-native": ["error"],
         "no-extra-bind": ["error"],
+        //"no-extra-label"
         "no-fallthrough": ["error"],
         "no-floating-decimal": ["error"],
         "no-implicit-coercion": ["error", {
@@ -64,10 +72,12 @@ module.exports = {
         }],
         "no-implicit-globals": ["error"],
         "no-implied-eval": ["error"],
+        //"no-invalid-this"
         "no-iterator": ["error"],
         "no-labels": ["error"],
         "no-lone-blocks": ["error"],
         "no-loop-func": ["error"],
+        //"no-magic-numbers"
         "no-multi-spaces": ["error"],
         "no-multi-str": ["error"],
         "no-native-reassign": ["error"],
@@ -76,23 +86,30 @@ module.exports = {
         "no-new-wrappers": ["error"],
         "no-octal": ["error"],
         "no-octal-escape": ["error"],
+        //"no-param-reassign"
         "no-proto": ["error"],
         "no-redeclare": ["error"],
         "no-return-assign": ["error"],
         "no-script-url": ["error"],
         "no-self-assign": ["error"],
+        //"no-self-compare"
         "no-sequences": ["error"],
         "no-throw-literal": ["error"],
         "no-unmodified-loop-condition": ["error"],
         "no-unused-expressions": ["error"],
+        //"no-unused-labels"
         "no-useless-call": ["error"],
         "no-useless-concat": ["error"],
         "no-useless-escape": ["error"],
+        "no-void": ["off"],
         "no-warning-comments": ["error"],
         "no-with": ["error"],
         "radix": ["error"],
         "vars-on-top": ["error"],
         "yoda": ["error", "never"],
+
+        // Strict Mode
+        "strict": ["error"],
 
         // Variables
         "init-declarations": ["error", "never", {
@@ -100,12 +117,58 @@ module.exports = {
         }],
         "no-catch-shadow": ["error"],
         "no-delete-var": ["error"],
+        //"no-label-var"
+        //"no-restricted-globals"
+        //"no-shadow"
+        //"no-shadow-restricted-names"
+        //"no-undef"
+        //"no-undef-init"
+        //"no-undefined"
+        //"no-unused-vars"
+        //"no-use-before-define"
 
-        // Style
+        // Node.js and CommonJS
+        //"callback-return"
+        "global-require": ["error"],
+        //"handle-callback-err"
+        "no-mixed-requires": ["error"],
+        "no-new-require": ["error"],
+        "no-path-concat": ["off"],
+        "no-process-env": ["error"],
+        "no-process-exit": ["error"],
+        "no-restricted-modules": ["off"],
+        "no-sync": ["error"],
+
+        // Stylistic Issues
+        "array-bracket-spacing": ["error", "always"],
+        "block-spacing": ["error", "always"],
+        "brace-style": ["error", "1tbs", {
+            "allowSingleLine": true,
+        }],
+        "camelcase": ["error", {
+            "properties": "always",
+        }],
+        "comma-dangle": ["error", "always-multiline"],
+        "comma-spacing": ["error", {
+            "before": false,
+            "after": true,
+        }],
+        "comma-style": ["error", "last"],
+        //"computed-property-spacing"
+        //"consistent-this"
+        //"eol-last"
+        //"func-names"
+        //"func-style"
+        //"id-blacklist"
+        //"id-length"
+        //"id-match"
+        //"indent"
+        //"jsx-quotes"
         "key-spacing": ["error", {
             "beforeColon": false,
             "afterColon": true,
             "mode": "minimum",
         }],
+        //...
     },
 };
