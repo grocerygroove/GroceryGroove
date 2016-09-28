9c24d9dc3a3684be53968dd29d2c080ffc1fcf30
Shawn Dellysse <shawnd@omniti.com>
2016-09-28 00:44:26 -0400

    added guard to utils/reset-testing-database

--- apps/api/src/utils/reset-testing-database.js
+++ apps/api/src/utils/reset-testing-database.js
@@ -2,6 +2,10 @@ const a = require("./asyncify");
 const execp = require("./execp");
 
 module.exports = a(function* resetTestingDatabase () {
+    if (process.env.NODE_ENV !== "development") {
+        throw new Error("must be in dev env to reset test db");
+    }
+
     yield execp(`/bin/migrate downtesting`);
     yield execp(`/bin/migrate uptesting`);
 });
