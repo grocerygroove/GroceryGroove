781129d1125bd0955f5255f12a1e1cec04fb1bb8
Jared Johnson <jaredj@gmx.com>
2016-10-02 14:24:50 -0400

    More tests

---
+++ apps/api/src/db/queries/categories/.get-all-names.test.js
@@ -0,0 +1,113 @@
+require('dotenv').load();
+global.Promise = require("bluebird");
+const a = require("../../../utils/asyncify");
+const makeDatabaseReal = require("../../make-database");
+const resetTestingDb = require("../../../utils/reset-testing-database");
+const defaultTestUser = require("../../../utils/default-test-user");
+const queries = require("../../queries");
+
+const makeDatabase = makeDatabaseReal.bind(null, {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    name: process.env.TEST_DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+});
+
+const tap = require("tap");
+
+tap.test("db/queries/categories/get-all-names", tap => {
+    const logger = {
+        info: () => {},
+        child: () => { return logger; },
+    };
+
+    const sortByName = (a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else if (a.name < b.name) {
+            return -1;
+        }
+        return 0;
+    };
+    const defaultCategories = [
+        {
+            "name": "Beverages",
+        },
+        {
+            "name": "Bread/Bakery",
+        },
+        {
+            "name": "Canned Goods",
+        },
+        {
+            "name": "Dairy",
+        },
+        {
+            "name": "Baking Goods",
+        },
+        {
+            "name": "Frozen Foods",
+        },
+        {
+            "name": "Meat",
+        },
+        {
+            "name": "Produce",
+        },
+        {
+            "name": "Home Goods",
+        },
+        {
+            "name": "Personal Care",
+        },
+        {
+            "name": "Other",
+        },
+    ].sort(sortByName);
+
+
+    tap.test("get-all-names default", a(function* (tap) {
+        yield resetTestingDb();
+
+        const db = makeDatabase();
+
+        const queriedRows = (yield queries.categories.getAllNames(db, logger, [
+            1,
+        ])).asPlainObjects();
+
+        tap.strictSame(queriedRows, defaultCategories);
+
+        yield db.end();
+    }));
+
+    tap.test("add one then get-all-names", a(function* (tap) {
+        yield resetTestingDb();
+
+        const db = makeDatabase();
+        const testCategoryName = "test category";
+        yield queries.categories.addOne(db, logger, [
+            defaultTestUser.primary_household_id,
+            defaultTestUser.user_id,
+            testCategoryName,
+        ]);
+
+        let expected;
+        expected = defaultCategories.slice();
+        expected.push({
+            "name": testCategoryName,
+        });
+        expected = expected.sort(sortByName);
+
+        const queriedRows = (yield queries.categories.getAllNames(db, logger, [
+            defaultTestUser.user_id,
+        ])).asPlainObjects();
+
+        tap.strictSame(queriedRows, expected);
+
+        yield db.end();
+    }));
+
+
+    tap.end();
+});
---
+++ apps/api/src/db/queries/grocery-lists/.add-one.test.js
@@ -0,0 +1,92 @@
+require('dotenv').load();
+global.Promise = require("bluebird");
+const a = require("../../../utils/asyncify");
+const makeDatabaseReal = require("../../make-database");
+const resetTestingDb = require("../../../utils/reset-testing-database");
+const defaultTestUser = require("../../../utils/default-test-user");
+const queries = require("../../queries");
+const DuplicateNameError = require("../../../errors/duplicate-name-error");
+
+const makeDatabase = makeDatabaseReal.bind(null, {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    name: process.env.TEST_DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+});
+
+const tap = require("tap");
+
+tap.test("db/queries/grocery-lists/add-one", tap => {
+    const logger = {
+        info: () => {},
+        child: () => { return logger; },
+    };
+
+    tap.test("clean insert", a(function* (tap) {
+        yield a(function* () {
+            yield resetTestingDb();
+
+            const db = makeDatabase();
+
+            const testGroceryListName = "Test List";
+
+            yield queries.groceryLists.addOne(db, logger, [
+                defaultTestUser.user_id,
+                testGroceryListName,
+                defaultTestUser.primary_household_id,
+            ]);
+
+            const rows = (yield db.query(logger, `
+                SELECT COUNT(*) AS count
+                FROM grocery_lists
+                WHERE name = '${testGroceryListName}'
+                    AND household_id = '${defaultTestUser.primary_household_id}'
+                    AND created_by_id = '${defaultTestUser.user_id}'
+            `)).asPlainObjects();
+
+            const actual = parseInt(rows[0].count, 10);
+            const expected = 1;
+
+            tap.strictEquals(actual, expected);
+
+            yield db.end();
+        })();
+
+    }));
+
+    tap.test("duplicate insert throws DuplicateNameError", a(function* (tap) {
+        yield a(function* () {
+            yield resetTestingDb();
+
+            const db = makeDatabase();
+
+            const testGroceryListName = "Test List";
+
+            //Add grocery list, first insert should be good
+            yield queries.groceryLists.addOne(db, logger, [
+                defaultTestUser.user_id,
+                testGroceryListName,
+                defaultTestUser.primary_household_id,
+            ]);
+
+            //Second should throw a DuplicateNameError
+            try {
+                yield queries.groceryLists.addOne(db, logger, [
+                    defaultTestUser.user_id,
+                    testGroceryListName,
+                    defaultTestUser.primary_household_id,
+                ]);
+            } catch (e) {
+                tap.type(e, 'DuplicateNameError', "Duplicate category insert throws DuplicateNameError");
+            }
+
+            yield db.end();
+        })();
+
+    }));
+
+
+
+    tap.end();
+});
