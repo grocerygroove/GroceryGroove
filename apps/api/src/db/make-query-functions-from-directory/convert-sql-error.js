17adeb8ba8fb65313d42f44aa69ec0185f097a9c
Jared Johnson <jaredj@gmx.com>
2016-10-02 11:53:58 -0400

    I'm guessing newer postgres version changed the property name from sqlState to code. Updated code to match

--- apps/api/src/db/make-query-functions-from-directory/convert-sql-error.js
+++ apps/api/src/db/make-query-functions-from-directory/convert-sql-error.js
@@ -1,14 +1,14 @@
 module.exports = function convertSqlError (pathname, handlers, error) {
     if (handlers) {
-        if (error.sqlState && handlers.states[error.sqlState]) {
-            const ErrorClass = handlers.states[error.sqlState];
+        if (error.code && handlers.states[error.code]) {
+            const ErrorClass = handlers.states[error.code];
 
             return new ErrorClass(error, pathname);
         } else if (error.sqlState) {
-            return new Error(`Sql Error ${error.sqlState} not caught for ${pathname}`);
+            return new Error(`Sql Error ${error.code} not caught for ${pathname}`);
         }
     } else if (error.sqlState) {
-            return new Error(`Sql Error ${error.sqlState} not caught for ${pathname}`);
+            return new Error(`Sql Error ${error.code} not caught for ${pathname}`);
     }
 
     return void(0);
