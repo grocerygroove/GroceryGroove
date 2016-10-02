744aa579da1715407ccc21aa22b4588c92a8ddf1
Jared Johnson <jaredj@gmx.com>
2016-10-02 11:54:33 -0400

    Added another add one categories test

--- apps/api/src/db/queries/categories/.add-one.test.js
+++ apps/api/src/db/queries/categories/.add-one.test.js
@@ -4,6 +4,7 @@ const a = require("../../../utils/asyncify");
 const makeDatabaseReal = require("../../make-database");
 const resetTestingDb = require("../../../utils/reset-testing-database");
 const queries = require("../../queries");
+const DuplicateNameError = require("../../../errors/duplicate-name-error");
 
 const makeDatabase = makeDatabaseReal.bind(null, {
     user: process.env.DB_USER,
@@ -15,13 +16,13 @@ const makeDatabase = makeDatabaseReal.bind(null, {
 
 const tap = require("tap");
 
-tap.test("db/queries/categories", tap => {
+tap.test("db/queries/categories/add-one", tap => {
     const logger = {
         info: () => {},
         child: () => { return logger; },
     };
 
-    tap.test("add-one", a(function* (tap) {
+    tap.test("clean insert", a(function* (tap) {
 
         yield a(function* () {
             yield resetTestingDb();
@@ -51,6 +52,42 @@ tap.test("db/queries/categories", tap => {
             const actual = rows[0];
             const expected = testCategory;
             tap.strictSame(actual, expected, "Add a category");
+
+            yield db.end();
+        })();
+    }));
+
+    tap.test("duplicate insert", a(function* (tap) {
+
+        yield a(function* () {
+            yield resetTestingDb();
+
+            const db = makeDatabase();
+
+            const testCategory = {
+                "category_id": 12,
+                "household_id": null,
+                "name": "testcategory",
+                "created_by_id": null,
+            };
+
+            //Add a category, first insert should be good
+            yield queries.categories.addOne(db, logger, [
+                testCategory.household_id,
+                testCategory.created_by_id,
+                testCategory.name,
+            ]);
+
+            //Second should throw a DuplicateNameError
+            try {
+                const result = yield queries.categories.addOne(db, logger, [
+                            testCategory.household_id,
+                            testCategory.created_by_id,
+                            testCategory.name,
+                    ]);
+            } catch (e) {
+                tap.type(e, 'DuplicateNameError', "Duplicate category insert throws DuplicateNameError");
+            }
 
             yield db.end();
         })();
