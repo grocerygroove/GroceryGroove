9b46b850e5388371e8287f99eb3cfa7f0bde616c
Jared Johnson <jaredj@gmx.com>
2016-10-02 22:05:55 -0400

    Initial work on households add one test

---
+++ apps/api/src/db/queries/households/.add-one.test.js
@@ -0,0 +1,61 @@
+require('dotenv').load();
+global.Promise = require("bluebird");
+const a = require("../../../utils/asyncify");
+const makeDatabaseReal = require("../../make-database");
+const resetTestingDb = require("../../../utils/reset-testing-database");
+const defaultTestUser = require("../../../utils/default-test-user");
+const queries = require("../../queries");
+const DuplicateNameError = require("../../../errors/duplicate-name-error");
+
+const makeDatabase = makeDatabaseReal.bind(null, {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    name: process.env.TEST_DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+});
+
+const tap = require("tap");
+
+tap.test("db/queries/households/add-one", tap => {
+    const logger = {
+        info: () => {},
+        child: () => { return logger; },
+    };
+
+    tap.test("add one", a(function* (tap) {
+        yield a(function* () {
+            yield resetTestingDb();
+
+            const db = makeDatabase();
+            const householdName = "Test House";
+
+            const householdId = yield queries.households.addOne(db, logger, [
+                defaultTestUser.user_id,
+                householdName,
+            ]);
+
+            const expected = {
+                "household_id": 2,
+                "name": householdName,
+                "created_by_id": defaultTestUser.user_id,
+                "household_admin": defaultTestUser.user_id,
+            };
+
+            const actual = (yield db.query(logger, `
+                SELECT *
+                FROM households
+                WHERE household_id = '${householdId}'
+            `)).asPlainObjects();
+
+            console.log(actual);
+
+            tap.strictDeepEquals(actual, expected);
+
+            yield db.end();
+        })();
+    }));
+
+
+    tap.end();
+});
