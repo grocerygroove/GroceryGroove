d2214ccd9ef24c2586d0911e7dc1ce015ce614e4
Shawn Dellysse <shawnd@omniti.com>
2016-11-07 22:57:55 -0500

    added better error to database-files

--- apps/api/local_modules/database-files/apply-row-filter.js
+++ apps/api/local_modules/database-files/apply-row-filter.js
@@ -1,9 +1,11 @@
-const InvalidRowFilterType = require("./invalid-row-filter-error");
+const InvalidRowFilterError = require("./errors/invalid-row-filter-error");
 
 const rowFilters = {
     column: function (rows) {
-        return rows.map(row => row[Object.keys(row)[0]]);
+        let firstKey = null;
+        return rows.map(row => row[firstKey || (firstKey = Object.keys(row)[0])]);
     },
+
     one: function (rows) {
         const row = rows[0];
         if (row) {
@@ -12,16 +14,18 @@ const rowFilters = {
             return void(0);
         }
     },
+
     row: function (rows) {
         return rows[0];
     },
 };
+
 module.exports = function applyRowFilter (type, rows) {
     if (type) {
         if (rowFilters[type]) {
             return rowFilters[type](rows);
         } else {
-            throw new InvalidRowFilterType(type);
+            throw new InvalidRowFilterError(type);
         }
     } else {
         return rows;
--- apps/api/local_modules/database-files/index.js
+++ apps/api/local_modules/database-files/index.js
@@ -1,4 +1,8 @@
 module.exports = {
     createQueriesFromDirectory: require("./create-queries-from-directory"),
     createTransactionsFromDirectory: require("./create-transactions-from-directory"),
+
+    errors: {
+        InvalidRowFilterError: require("./errors/invalid-row-filter-error"),
+    },
 };
