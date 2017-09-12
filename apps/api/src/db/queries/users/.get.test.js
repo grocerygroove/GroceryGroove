e1b5704a93fd7fe85728372966056d1ab2864641
Jared Johnson <jaredj@gmx.com>
2017-09-11 22:17:58 -0400

    Added get user households tests

--- apps/api/src/db/queries/users/.get.test.js
+++ apps/api/src/db/queries/users/.get.test.js
@@ -5,6 +5,7 @@ const Pool = require('pg').Pool;
 const queries = require("../../queries");
 const resetTestingDb = require("../../../utils/reset-testing-database");
 const tap = require("tap");
+const secondaryUser = require("../../../utils/secondary-test-user");
 
 const logger = {
   info: () => {},
@@ -37,6 +38,59 @@ tap.test("db/queries/user/get-one", tap => {
     };
 
     tap.same(user, expected);
+
+    await db.end();
+  }));
+
+  tap.end();
+});
+
+tap.test("db/queries/user/getUserHouseholds", tap => {
+  tap.test("get households: scenario: 1", (async (tap) => {
+    await resetTestingDb();
+
+    const db = new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.TEST_DB_NAME,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+    });
+
+    const households = await queries.users.getUserHouseholds(db, logger, {
+      userId: defaultUser.user_id,
+    });
+
+    const expected = [ 1 ];
+
+    tap.same(households, expected);
+
+    await db.end();
+  }));
+
+  tap.test("get households: scenario: 2", (async (tap) => {
+    await resetTestingDb();
+
+    const db = new Pool({
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.TEST_DB_NAME,
+      port: process.env.DB_PORT,
+      host: process.env.DB_HOST,
+    });
+
+    await queries.users.createHouseholdUser(db, logger, {
+      userId: defaultUser.user_id,
+      householdId: secondaryUser.primary_household_id,
+    });
+
+    const households = await queries.users.getUserHouseholds(db, logger, {
+      userId: defaultUser.user_id,
+    });
+
+    const expected = [ 1, 2 ];
+
+    tap.same(households, expected);
 
     await db.end();
   }));
--- apps/api/src/db/queries/users/get-user-households.sql
+++ apps/api/src/db/queries/users/get-user-households.sql
@@ -1,4 +1,11 @@
+{
+  namedParameters: {
+    enabled: true,
+  },
+
+  returns: "column",
+}
 SELECT household_id
 FROM households_users
-WHERE user_id = $1
+WHERE user_id = :userId
 ORDER BY household_id
